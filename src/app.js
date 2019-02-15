0991bbbd816dc5c395e2ad5f2284bacd1d3e937c
syneva.j.runyan <syneva.j.runyan@accenture.com>
2019-02-15 16:24:33 -0500

    explicitly specifiying file encoding

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ app.post("/transfer", (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts)
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON);
-  res.render("transfer", { message: "Transfer completed"});
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
+  res.render("transfer", { message: "Transfer Completed"});
 });
 
 app.get("/payment", (req, res) => res.render('payment', { account: accounts.credit }));
@@ -37,7 +37,7 @@ app.post("/payment", (req, res) => {
   accounts.credit.balance = accounts.credit.balance - req.body.amount;
   accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
   const accountsJSON = JSON.stringify(accounts)
-  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON);
+  fs.writeFileSync(path.join(__dirname, "json", "accounts.json"), accountsJSON, 'utf8');
   res.render("payment", { message: "Payment Successful", account: accounts.credit });
 });
 
