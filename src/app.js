b3fdb902aaa0a9c935c23bd2804f5306eeabc4d1
syneva.j.runyan <syneva.j.runyan@accenture.com>
2019-02-15 15:43:55 -0500

    completed module 2

--- src/app.js
+++ src/app.js
@@ -4,13 +4,24 @@ const express = require("express");
 
 const app = express();
 
+// Account data
+const accountData = fs.readFileSync("./src/json/accounts.json", "utf8");
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("./src/json/users.json", "utf8");
+const users = JSON.parse(userData);
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render('index', { title: "Index" }));
+app.get("/", (req, res) => res.render('index', { title: "Account Summary", accounts }));
+app.get("/savings", (req, res) => res.render('account', { account: accounts.savings }));
+app.get("/credit", (req, res) => res.render('account', { account: accounts.credit }));
+app.get("/checking", (req, res) => res.render('account', { account: accounts.checking }));
+app.get("/profile", (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => {
   console.log("PS Project is running on port 3000!");
